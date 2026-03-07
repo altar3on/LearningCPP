3f53c7b81870fa145da7547a268c6d5eb7af2465
Altareon <anshmishra16080@gmail.com>
2026-03-07 20:19:27 +0530

    Interactivity!

---
+++ Fundamentals/taking_inputs.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() 
+{
+    std::cout << "Time Travel\n";
+    std::cout << "Fast Travel to a century between 1500 - 1600: ";
+
+    int century {};
+    std::cin >> century;
+
+    std::cout << "Travelling to " << century << "...";
+}
